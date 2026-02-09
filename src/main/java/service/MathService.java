a72212185f4bc97c47b808eaab6ebae0e6566b62
Jav8-Dev <javiergarrochomunoz@gmail.com>
2026-02-09 14:09:28 +0100

    hlis

--- src/main/java/service/MathService.java
+++ src/main/java/service/MathService.java
@@ -1,5 +1,8 @@
 package service;
 
 public class MathService {
+    public int sumar(int a, int b){
+        return a + b;
+    }
 }
 //@author Jav8-dev|@version 1.0.
--- src/test/java/MathServiceTest.java
+++ src/test/java/MathServiceTest.java
@@ -1,5 +1,20 @@
-package PACKAGE_NAME;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import service.MathService;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MathServiceTest {
+    private MathService mathService;
+
+    @BeforeEach
+    void setUp(){
+        mathService =new MathService();
+    }
+    @Test
+    public void TestSuma(){
+        int a = 5;
+        int b = 4;
+        int result = this.mathService.sumar(a,b);
+    }
 }
 //@author Jav8-dev|@version 1.0.
